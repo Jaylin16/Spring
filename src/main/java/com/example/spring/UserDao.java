2b651d2a621653c373bd1e92d438d55bdd9f03d6
jaylin <jaylin6115@gmail.com>
2023-05-26 23:31:58 +0900

    [1장] 1.2.2. User DAO의 '관심사 1. DB연결'에 대한 부분을 리팩토링을 통해 관심사 분리를 하고 테스트 검증 진행 (리팩토링 - 테스트 검증은 세트)

--- src/main/java/com/example/spring/UserDao.java
+++ src/main/java/com/example/spring/UserDao.java
@@ -6,8 +6,7 @@ public class UserDao {
     public void add(User user) throws ClassNotFoundException, SQLException {
 
         //관심사 1. DB연결
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost", "유저이름", "비밀번호");
+        Connection c = getConnection();
 
         //관심사 2. 사용자 등록을 위한 SQL문장을 담을 statement 생성 및 실행
         PreparedStatement ps = c.prepareStatement("insert into `user`.users(id, name, password) values(?,?,?)");
@@ -23,8 +22,7 @@ public class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost", "유저이름", "비밀번호");
+        Connection c = getConnection();
 
         PreparedStatement ps = c.prepareStatement("select * from `user`.users WHERE id = ?");
         ps.setString(1, id);
@@ -43,12 +41,20 @@ public class UserDao {
         return user;
     }
 
+    //관심사 분리. '관심사 1 - DB연결'에 대한 기능을 리팩토링의 "메소드 추출 기법"을 통해 관심사 분리
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost", "유저이름", "비밀번호");
+
+        return c;
+    }
+
     //테스트용 main() 메소드 작성
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         UserDao dao = new UserDao();
 
         User user = new User();
-        user.setId("test");
+        user.setId("test2");
         user.setName("me");
         user.setPassword("choco");
 
